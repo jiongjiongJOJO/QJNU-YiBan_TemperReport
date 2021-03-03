9fa5e2b9a2b4bf9124718eab95a9fa2408c1c57c
囧囧JOJO <41473909@qq.com>
2021-03-03 19:32:06 +0800

    Update util.py

--- util.py
+++ util.py
@@ -2,26 +2,26 @@ import datetime
 
 def get_time():
     now = datetime.datetime.now()
-    delta = datetime.timedelta(hours=-8)
+    delta = datetime.timedelta(hours=8)
     n_days = now + delta
     return (n_days.strftime("%Y-%m-%d %H:%M:%S"))
 
 def get_time_no_second():
     now = datetime.datetime.now()
-    delta = datetime.timedelta(hours=-8)
+    delta = datetime.timedelta(hours=8)
     n_days = now + delta
     return (n_days.strftime("%Y-%m-%d %H:%M"))
 
 def get_7_day_ago():
     now = datetime.datetime.now()
-    delta = datetime.timedelta(days=-7,hours=-8)
+    delta = datetime.timedelta(days=-7,hours=8)
     n_days = now + delta
     return n_days.strftime('%Y-%m-%d')
 
 
 def get_today():
     now = datetime.datetime.now()
-    delta = datetime.timedelta(hours=-8)
+    delta = datetime.timedelta(hours=8)
     n_days = now + delta
     return n_days.strftime("%Y-%m-%d")
 
